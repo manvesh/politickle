65131abb0ba3fcb9b7b437fb170e87122f81eaea
Matt Gadda <mgadda@twitter.com>
2014-01-06 15:51:52 -0800

    update PollsController.show with logic to render poll

--- app/controllers/PollsController.scala
+++ app/controllers/PollsController.scala
@@ -2,19 +2,17 @@ package controllers
 
 import play.api.mvc.Controller
 import play.api.db.slick.DBAction
+import play.api.db.slick._
+import play.api.db.slick.Config.driver.simple._
+import play.api.data._
 import play.api.Play.current
+import models.Polls
 
 object PollsController extends Controller with securesocial.core.SecureSocial {
   def show(id: Long) = DBAction { implicit request =>
-    // TODO:
-    /*
     Polls.findById(id) map { poll =>
-      Ok(views.html.Polls.show(poll)
-    } getOrlElse(NotFound)
-
-     */
-    val poll = 0
-    Ok(views.html.Polls.show(id))
+      Ok(views.html.Polls.show(poll))
+    } getOrElse(NotFound)
   }
 
   def create = SecuredAction { implicit request =>
--- app/controllers/ResponsesController.scala
+++ app/controllers/ResponsesController.scala
@@ -5,6 +5,7 @@ import play.api.libs.json.Json
 
 object ResponsesController extends Controller with securesocial.core.SecureSocial {
   def create(id: Long) = SecuredAction { implicit request =>
+    // TODO: Create Response
     Ok(Json.toJson(Map("binding_values" -> "go_here"))).as("application/json")
   }
 }
