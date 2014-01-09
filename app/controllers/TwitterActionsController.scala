9c0f844dc653a4d69146d05340f7efb019771f26
Ashish Virmani <virmani.ashish@gmail.com>
2014-01-09 00:25:04 -0800

    added and tested lookupUsers. Added (untested) postTweet

---
+++ app/controllers/TwitterActionsController.scala
@@ -0,0 +1,93 @@
+package controllers
+
+import play.api.mvc.{Action, Request, SimpleResult, Controller}
+import play.api.db.slick.DBAction
+import play.api.db.slick._
+import play.api.data._
+import play.api.data.Mapping
+import play.api.data.Forms._
+import play.api.Play.current
+import models._
+import play.Logger
+import scala.concurrent.Future
+import securesocial.core.{SecureSocial, SecuredRequest}
+import scala.Some
+import models.Choice
+import securesocial.core.SecuredRequest
+import models.Poll
+import util.TwitterHelper
+import play.api.libs.json.Json
+import scala.collection.JavaConverters._
+import java.util.{List => JList}
+import twitter4j.User
+
+
+object TwitterActionsController extends Controller with securesocial.core.SecureSocial {
+
+  case class HandleList(handles: List[String])
+
+  case class Tweet(tweet: String)
+
+  val handlesForm = Form(
+    mapping(
+      "handles" -> list(text)
+    )(HandleList.apply)(HandleList.unapply)
+  )
+
+  val tweetForm = Form(
+    mapping(
+      "tweet" -> text
+    )(Tweet.apply)(Tweet.unapply)
+  )
+
+  def lookupUser() = SecuredAction(ajaxCall = true) {
+    implicit request =>
+      val handles = handlesForm.bindFromRequest.get
+      Logger.debug("handles here: " + handles.handles.mkString(", "))
+      DB withSession {
+        implicit s: Session =>
+          val userFromDB = Users.findByTwitterId(request.user.identityId.userId)
+          userFromDB map {
+            user =>
+              val twitterOption = TwitterHelper.getTwitter4JInstance(user)
+
+              twitterOption map {
+                twitter =>
+                  val users = twitter.lookupUsers(handles.handles.toArray)
+                    .asInstanceOf[JList[User]].asScala map {
+                    user =>
+                      user.getId()
+                  }
+                  Ok(Json.toJson(users))
+              } getOrElse (Unauthorized("Don't have no twitter creds."))
+
+          } getOrElse (Unauthorized("User not logged in"))
+      }
+  }
+
+  def postTweet() = SecuredAction(ajaxCall = true) {
+    implicit request =>
+      val tweet = tweetForm.bindFromRequest.get
+
+      DB withSession {
+        implicit s: Session =>
+          val userFromDB = Users.findByTwitterId(request.user.identityId.userId)
+          userFromDB map {
+            user =>
+              val twitterOption = TwitterHelper.getTwitter4JInstance(user)
+
+              twitterOption map {
+                twitter =>
+                  twitter.updateStatus(tweet.tweet)
+                  Ok(Json.toJson(
+                    Map(
+                      "success" -> "true"
+                    )
+                  ))
+              } getOrElse (Unauthorized("Don't have no twitter creds."))
+
+          } getOrElse (Unauthorized("User not logged in"))
+      }
+  }
+
+}
--- app/util/TwitterHelper.scala
+++ app/util/TwitterHelper.scala
@@ -3,20 +3,24 @@ package util
 import twitter4j.conf.ConfigurationBuilder
 import twitter4j.{Twitter, TwitterFactory}
 import models.User
+import play.Play
 
 
 object TwitterHelper {
   def getTwitter4JInstance(user: User) = {
     for (accessToken <- user.accessToken; secret <- user.secret)
     yield {
+      val twitterKey = Play.application().configuration().getString("securesocial.twitter.consumerKey")
+      val twitterSecret = Play.application().configuration().getString("securesocial.twitter.consumerSecret")
+
       val cb = new ConfigurationBuilder()
       cb.setDebugEnabled(true)
-        .setOAuthConsumerKey("*********************")
-        .setOAuthConsumerSecret("******************************************")
+        .setOAuthConsumerKey(twitterKey)
+        .setOAuthConsumerSecret(twitterSecret)
         .setOAuthAccessToken(accessToken)
         .setOAuthAccessTokenSecret(secret)
       val tf = new TwitterFactory(cb.build())
-      val twitter = tf.getInstance()
+      tf.getInstance()
     }
   }
 }
